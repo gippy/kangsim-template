608e85d0381fd1ce27f692d7956850cb55999d5c
Jaroslav Hejlek <hejlekjaroslav@gmail.com>
2021-05-19 20:37:30 +0200

    Correct build

--- docs/main.js
+++ docs/main.js
@@ -1,61 +1,58 @@
-
-(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
 (function () {
     'use strict';
 
     let modal = null;
 
     function setInnerHTML(parent, html) {
-        parent.innerHTML = '';
-        parent.innerHTML = html;
-        const scripts = parent.querySelectorAll('script');
-        scripts.forEach((script) => {
-            // Dangerous, but only way to eval scripts loaded externally
-            eval(script.innerHTML);
-        });
+      parent.innerHTML = '';
+      parent.innerHTML = html;
+      const scripts = parent.querySelectorAll('script');
+      scripts.forEach(script => {
+        // Dangerous, but only way to eval scripts loaded externally
+        eval(script.innerHTML);
+      });
     }
 
     async function openInModal(event, link) {
-        // Do not open links to pages that should open in new window in modal
-        if (link.target === '_blank') return;
+      // Do not open links to pages that should open in new window in modal
+      if (link.target === '_blank') return; // If link starts with # it's a link inside the main page, skip it too.
 
-        // If link starts with # it's a link inside the main page, skip it too.
-        const href = link.getAttribute('href');
-        if (href.startsWith('#')) return;
+      const href = link.getAttribute('href');
+      if (href.startsWith('#')) return; // Stop the normal link event
 
-        // Stop the normal link event
-        event.preventDefault();
+      event.preventDefault(); // Download the html content of the new page
 
-        // Download the html content of the new page
-        let content = '';
-        try {
-            const response = await fetch(link.href);
-            content = await response.text();
-        } catch (error) {
-            // If content load failed, log error in console and exit
-            console.error(error);
-            return;
-        }
+      let content = '';
 
-        const modalContentElement = document.querySelector('#page-content-modal .modal-actual-body');
-        // If for some reason the modal is not part of DOM log error and exit
-        if (!modalContentElement) {
-            console.error(`Document element with selector '${modalContentElement}' does not exist.`);
-            return;
-        }
-        setInnerHTML(modalContentElement, content);
-        // If the new modal content contains links, ensure that they also open in this modal
-        modalContentElement.querySelectorAll('a').forEach((link) => {
-            link.addEventListener('click', (event) => openInModal(event, link));
-        });
-        modal.show();
+      try {
+        const response = await fetch(link.href);
+        content = await response.text();
+      } catch (error) {
+        // If content load failed, log error in console and exit
+        console.error(error);
+        return;
+      }
+
+      const modalContentElement = document.querySelector('#page-content-modal .modal-actual-body'); // If for some reason the modal is not part of DOM log error and exit
+
+      if (!modalContentElement) {
+        console.error(`Document element with selector '${modalContentElement}' does not exist.`);
+        return;
+      }
+
+      setInnerHTML(modalContentElement, content); // If the new modal content contains links, ensure that they also open in this modal
+
+      modalContentElement.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', event => openInModal(event, link));
+      });
+      modal.show();
     }
 
     window.addEventListener('load', () => {
-        modal = new bootstrap.Modal(document.getElementById('page-content-modal'), {});
-        document.querySelectorAll('a').forEach((link) => {
-            link.addEventListener('click', (event) => openInModal(event, link));
-        });
+      modal = new bootstrap.Modal(document.getElementById('page-content-modal'), {});
+      document.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', event => openInModal(event, link));
+      });
     });
 
     let loadedSlides = 0;
@@ -63,123 +60,120 @@
     let currentSliderOffset = 0;
     let displaysMultiple = window.outerWidth > 991;
     let resizeTimeout;
-
     /**
      * Tahle funkce nacita novinky z endpointu nastaveneho do attributu `data-additional-news-href` u elementu `.posouvaci-novinky`
      * Vzdy odesle dotaz v tomto formatu `<data-additional-news-href>?offset=<aktualne zobrazeny pocet novinek>`.
      * Pokud endpoint vrati prazdnou odpoved, tak se nastavi promenna `maybeHasMoreItems` na `false` a uz se dalsi novinky nenactou.
      * @returns {void}
      */
+
     async function loadMoreSlides() {
-        const slider = document.querySelector('.posouvaci-novinky');
-        const href = slider.getAttribute('data-additional-news-href');
-        if (!href) return;
+      const slider = document.querySelector('.posouvaci-novinky');
+      const href = slider.getAttribute('data-additional-news-href');
+      if (!href) return;
 
-        try {
-            const response = await fetch(`${href}?offset=${loadedSlides - 1}`);
-            const additionalSlides = await response.text();
-            const slidesRow = document.querySelector('.posouvaci-novinky-obsah .row');
-            slidesRow.innerHTML = slidesRow.innerHTML + additionalSlides;
-            const nextLoadedSlides = document.querySelectorAll('.posouvaci-novinky-obsah .row .col-lg-4').length;
-            // Dotaz vratil prazdnou stranku bez novinek, tim padem dalsi novinky uz nejsou dostupne
-            if (nextLoadedSlides === loadedSlides) maybeHasMoreItems = false;
-            loadedSlides = nextLoadedSlides;
-        } catch (error) {
-            console.error(error);
-        }
+      try {
+        const response = await fetch(`${href}?offset=${loadedSlides - 1}`);
+        const additionalSlides = await response.text();
+        const slidesRow = document.querySelector('.posouvaci-novinky-obsah .row');
+        slidesRow.innerHTML = slidesRow.innerHTML + additionalSlides;
+        const nextLoadedSlides = document.querySelectorAll('.posouvaci-novinky-obsah .row .col-lg-4').length; // Dotaz vratil prazdnou stranku bez novinek, tim padem dalsi novinky uz nejsou dostupne
+
+        if (nextLoadedSlides === loadedSlides) maybeHasMoreItems = false;
+        loadedSlides = nextLoadedSlides;
+      } catch (error) {
+        console.error(error);
+      }
     }
-
     /**
      * Posouva novinky doprava, kdyz narazi na posledni tak zkusi nacist dalsi novinky.
      * Pokud uz pri predchozim nacteni byla nactena posledni novinka, tak uz to tato funkce dal nezkousi.
      * @param {Event} event
      * @returns {void}
      */
+
+
     function nextSlide(event) {
-        event.preventDefault();
-        const maxOffset = displaysMultiple ? loadedSlides - 3 : loadedSlides - 1;
-        if (currentSliderOffset >= maxOffset) return;
-        const slidesRow = document.querySelector('.posouvaci-novinky-obsah .row');
-        currentSliderOffset += 1;
-        const offsetPercentage = displaysMultiple ? 33.33 : 100;
-        slidesRow.style.transform = `translate(${-currentSliderOffset * offsetPercentage}%, 0)`;
+      event.preventDefault();
+      const maxOffset = displaysMultiple ? loadedSlides - 3 : loadedSlides - 1;
+      if (currentSliderOffset >= maxOffset) return;
+      const slidesRow = document.querySelector('.posouvaci-novinky-obsah .row');
+      currentSliderOffset += 1;
+      const offsetPercentage = displaysMultiple ? 33.33 : 100;
+      slidesRow.style.transform = `translate(${-currentSliderOffset * offsetPercentage}%, 0)`; // If we are one item before the last slide, try to load more slides
 
-        // If we are one item before the last slide, try to load more slides
-        if (maybeHasMoreItems && currentSliderOffset === maxOffset) loadMoreSlides();
+      if (maybeHasMoreItems && currentSliderOffset === maxOffset) loadMoreSlides();
     }
-
     /**
      * Posouva novinky doleva, kdyz narazi na prvni tak uz dal neposouva a nereaguje na klikani.
      * @param {Event} event
      * @returns {void}
      */
-    function prevSlide(event) {
-        event.preventDefault();
-        if (currentSliderOffset === 0) return;
-        const slidesRow = document.querySelector('.posouvaci-novinky-obsah .row');
-        currentSliderOffset -= 1;
-        const offsetPercentage = displaysMultiple ? 33.33 : 100;
-        slidesRow.style.transform = `translate(${-currentSliderOffset * offsetPercentage}%, 0)`;
-    }
 
+
+    function prevSlide(event) {
+      event.preventDefault();
+      if (currentSliderOffset === 0) return;
+      const slidesRow = document.querySelector('.posouvaci-novinky-obsah .row');
+      currentSliderOffset -= 1;
+      const offsetPercentage = displaysMultiple ? 33.33 : 100;
+      slidesRow.style.transform = `translate(${-currentSliderOffset * offsetPercentage}%, 0)`;
+    }
     /**
      * Pri zmene velikosti okna vyresetuje aktualni posuvnik novinek. Tato funkce se vola aby se
      * zajistilo spravne zobrazeni novinek, pokud uzivatel zmeni velikost okna natolik, ze se prepne
      * layout z mobilu na desktop a naopak.
      * Aby se predeslo pretizeni CPU tak se tato funkce vola az 300ms po posledni zmene velikosti okna.
      */
+
+
     function debouncedResetSlider() {
-        if (resizeTimeout) window.clearTimeout(resizeTimeout);
-        resizeTimeout = window.setTimeout(() => {
-            displaysMultiple = window.outerWidth > 991;
-            const slidesRow = document.querySelector('.posouvaci-novinky-obsah .row');
-            currentSliderOffset = 0;
-            slidesRow.style.transform = `translate(${-currentSliderOffset * 33.33}%, 0)`;
-        }, 300);
+      if (resizeTimeout) window.clearTimeout(resizeTimeout);
+      resizeTimeout = window.setTimeout(() => {
+        displaysMultiple = window.outerWidth > 991;
+        const slidesRow = document.querySelector('.posouvaci-novinky-obsah .row');
+        currentSliderOffset = 0;
+        slidesRow.style.transform = `translate(${-currentSliderOffset * 33.33}%, 0)`;
+      }, 300);
     }
 
     function initSlider(slider) {
-        const controls = slider.querySelectorAll('.posouvaci-novinky-ovladani button');
-        controls[0].addEventListener('click', prevSlide);
-        controls[1].addEventListener('click', nextSlide);
-
-        loadedSlides = document.querySelectorAll('.posouvaci-novinky-obsah .row .col-lg-4').length;
-
-        window.addEventListener('resize', debouncedResetSlider);
+      const controls = slider.querySelectorAll('.posouvaci-novinky-ovladani button');
+      controls[0].addEventListener('click', prevSlide);
+      controls[1].addEventListener('click', nextSlide);
+      loadedSlides = document.querySelectorAll('.posouvaci-novinky-obsah .row .col-lg-4').length;
+      window.addEventListener('resize', debouncedResetSlider);
     }
 
     window.addEventListener('load', () => {
-        const slider = document.querySelector('.posouvaci-novinky');
-        if (!slider) return;
-        initSlider(slider);
+      const slider = document.querySelector('.posouvaci-novinky');
+      if (!slider) return;
+      initSlider(slider);
     });
 
-    const navbar = document.querySelector('header nav');
+    const navbar = document.querySelector('header nav'); // OnScroll event handler
 
-    // OnScroll event handler
     const onScroll = () => {
-        // Get scroll value
-        const scroll = document.documentElement.scrollTop;
+      // Get scroll value
+      const scroll = document.documentElement.scrollTop; // If scroll value is more than 0 - add class
 
-        // If scroll value is more than 0 - add class
-        if (scroll > 16) {
-            navbar.classList.add("scrolled");
-        } else {
-            navbar.classList.remove("scrolled");
-        }
-    };
+      if (scroll > 16) {
+        navbar.classList.add("scrolled");
+      } else {
+        navbar.classList.remove("scrolled");
+      }
+    }; // When window scroll this adds a scrolled class to the navbar
 
-    // When window scroll this adds a scrolled class to the navbar
-    window.addEventListener('scroll', onScroll);
 
-    // When user clicks on links in navbar and navbar is open, it automatically closes it (on mobile)
+    window.addEventListener('scroll', onScroll); // When user clicks on links in navbar and navbar is open, it automatically closes it (on mobile)
+
     const navLinks = document.querySelectorAll('.navbar-nav:not(.social) .nav-link');
     const menuToggle = document.querySelector('.navbar-collapse');
     const bsCollapse = new bootstrap.Collapse(menuToggle);
-    navLinks.forEach((link) => {
-        link.addEventListener('click', () => {
-            bsCollapse.hide();
-        });
+    navLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        bsCollapse.hide();
+      });
     });
 
 }());
