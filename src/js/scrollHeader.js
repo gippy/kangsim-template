dbcee96454cdd922589eac36bfc2f81c29d21e98
Jaroslav Hejlek <hejlekjaroslav@gmail.com>
2021-05-19 20:33:01 +0200

    Improve collapse on mobile

--- src/js/scrollHeader.js
+++ src/js/scrollHeader.js
@@ -13,5 +13,15 @@ const onScroll = () => {
     }
 }
 
-// Use either onScroll or throttledOnScroll
+// When window scroll this adds a scrolled class to the navbar
 window.addEventListener('scroll', onScroll)
+
+// When user clicks on links in navbar and navbar is open, it automatically closes it (on mobile)
+const navLinks = document.querySelectorAll('.navbar-nav:not(.social) .nav-link');
+const menuToggle = document.querySelector('.navbar-collapse');
+const bsCollapse = new bootstrap.Collapse(menuToggle)
+navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        bsCollapse.hide();
+    });
+});
